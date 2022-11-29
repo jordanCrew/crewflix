33d5cb75da1fe801fbde3adbd89aba5cd384c5f0
Jordan <jordan.crew10@gmail.com>
2022-11-29 16:44:54 +0000

    created the watch video page

--- app/watch.php
+++ app/watch.php
@@ -8,3 +8,15 @@ if(!isset($_GET["id"])) {
 $video = new Video($con, $_GET["id"]);
 $video->incrementViews();
 ?>
+<div class="watchContainer">
+    <div class="videoControls watchNav">
+        <button onclick="goBack()" class="transparent iconButton"><i class='fas fa-arrow-left'></i></button>
+        <h1><?php echo $video->getTitle()?></h1>
+
+    </div>
+
+    <video controls autoplay src='<?php echo $video->getFilePath();?>' type="video/mp4"></video>
+</div>
+<script>
+initVideo("<?php echo $video->getId(); ?>", "<?php echo $userLoggedIn; ?>");
+</script>
