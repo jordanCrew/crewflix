07438fff45c045a9729853fbd06a11cc7a7ad90b
Jordan <jordan.crew10@gmail.com>
2022-12-01 17:47:18 +0000

    account page

---
+++ app/profile.php
@@ -0,0 +1,97 @@
+<?php
+require_once("includes/header.php");
+
+$detailsMessage = "";
+$passwordMessage = "";
+
+if(isset($_POST["saveDetailsButton"])){
+    $account = new Account($con);
+
+    $firstName = FormSanitiser::santiseFormString($_POST["firstName"]);
+    $lastName = FormSanitiser::santiseFormString($_POST["lastName"]);
+    $email = FormSanitiser::santiseFormUsername($_POST["email"]);
+
+    if($account->updateDetails($firstName, $lastName, $email, $userLoggedIn)) {
+        $detialsMessage = "<div class='alertSuccess'>
+                                Details updated successfully!
+                            </div>";
+    }
+    else {
+        $errorMessage = $account->getFirstError();
+            $detialsMessage = "<div class='alertSuccess'>
+                                $errorMessage
+                            </div>";
+
+    }
+}
+
+if(isset($_POST["savePasswordButton"])){
+    $account = new Account($con);
+
+    $oldPassword = FormSanitiser::santiseFormPassword($_POST["oldPassword"]);
+    $newPassword = FormSanitiser::santiseFormPassword($_POST["newPassword"]);
+    $newPassword2 = FormSanitiser::santiseFormPassword($_POST["newPassword2"]);
+
+    if($account->updatePassword($oldPassword, $newPassword, $newPassword2, $userLoggedIn)) {
+        $passwordMessage = "<div class='alertSuccess'>
+                                Password updated successfully!
+                            </div>";
+    }
+    else {
+        $errorMessage = $account->getFirstError();
+            $passwordMessage = "<div class='alertSuccess'>
+                                $errorMessage
+                            </div>";
+
+    }
+}
+
+?>
+
+<div class="settingsContainer column">
+    <div class="formSection">
+        <form method="POST">
+            <h2>User details</h2>
+
+            <?php
+                $user = new User($con, $userLoggedIn);
+
+                $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : $user->getFirstName();
+                $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : $user->getLastName();
+                $email = isset($_POST["email"]) ? $_POST["email"] : $user->getEmail();
+
+            ?>
+
+            <input type="text" name="firstName" placeholder="First Name" value="<?php echo $firstName ?>">
+            <input type="text" name="lastName" placeholder="Last Name" value="<?php echo $lastName ?>">
+            <input type="email" name="email" placeholder="email" value="<?php echo $email ?>">
+            <div class="message"><?php $detialsMessage; ?></div>
+            <input type="submit" name="saveDetailsButton" value="Save">
+        </form>
+    </div>
+
+    <div class="formSection">
+        <form method="POST">
+            <h2>Update password</h2>
+            <input type="password" name="oldPassword" placeholder="Old password">
+            <input type="password" name="newPassword" placeholder="New password">
+            <input type="password" name="newPassword2" placeholder="Confirm new password">
+            <div class="message"><?php $passwordMessage; ?></div>
+            <input type="submit" name="savePasswordButton" value="Save ">
+        </form>
+    </div>
+
+    <div class="formSection">
+        <h2>Subscription</h2>
+
+        <?php 
+            if($user->getIsSubscribed()){
+                echo "<h3>You are subscribed! Go to PayPal to cancel.</h3>";
+            }
+
+            else{
+                echo " <a href='billing.php'>Subscribe to Crewflix</a>";
+            }
+        ?>
+    </div>
+</div>
